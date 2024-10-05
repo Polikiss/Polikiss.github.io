2d532042f53b55fca5aa24857e707ca49281d9e4
Polikiss <mymmypolly@gmail.com>
2024-10-05 14:01:45 +0300

    fix: SMACSS

--- tarotLove/js/time.js
+++ tarotLove/js/time.js
@@ -4,7 +4,7 @@
     window.addEventListener('load', function() {
         const endTime = performance.now();
         const loadTime = endTime - startTime;
-        const footer = document.getElementById("time");
+        const footer = document.getElementById("footer__time");
         const loadTimeInfo = document.createElement('p');
         loadTimeInfo.textContent = `Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
 
