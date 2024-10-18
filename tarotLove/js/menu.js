d0ca31ed11c7199917e3064feecc554f3db5981c
Polikiss <mymmypolly@gmail.com>
2024-10-18 22:28:01 +0300

    chore: add to-do

--- tarotLove/js/menu.js
+++ tarotLove/js/menu.js
@@ -4,6 +4,18 @@ function toggleDropdown() {
     document.getElementById("dropdown-content").classList.toggle('open');
 }
 
+window.onclick = function (event) {
+    if (!event.target.matches('#menu')) {
+        const dropdowns = document.getElementById("#dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('open')) {
+                openDropdown.classList.remove('open');
+            }
+        }
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     const tabs = document.querySelectorAll('.main-title-box__menu__menu-item__tab');
