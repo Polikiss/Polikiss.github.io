4ce43a31903771adee15e33bc4089178de033baf
Polikiss <mymmypolly@gmail.com>
2024-11-01 18:59:01 +0300

    chore: 7 lab glide

--- tarotLove/js/cards.js
+++ tarotLove/js/cards.js
@@ -1,33 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchData();
-});
-async function fetchData() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const data = await response.json();
-        renderData(data);
-    } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-        showError();
+import Glide from "@glidejs/glide";
+
+const glide = new Glide('#intro', {
+    type: 'carousel',
+    perView: 4,
+    focusAt: 'center',
+    breakpoints: {
+        800: {
+            perView: 2
+        },
+        480: {
+            perView: 1
+        }
     }
-}
-function renderData(data) {
-    const content = document.getElementById('content');
-    content.innerHTML = '';
+}).mount();
 
-    data.slice(0, 10).forEach(photo => { // Отображаем первые 10 фото
-        const photoDiv = document.createElement('div');
-        photoDiv.innerHTML = `
-            <img src="${photo.thumbnailUrl}" alt="${photo.title}">
-            <p>${photo.title}</p>
-        `;
-        content.appendChild(photoDiv);
-    });
-}
 
-function showError() {
-    const content = document.getElementById('content');
-    const errorDiv = document.createElement('div');
-    errorDiv.textContent = '⚠ Что-то пошло не так.';
-    content.appendChild(errorDiv);
-}
